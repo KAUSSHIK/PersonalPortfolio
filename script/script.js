218fc7a75f321ba66e451e4a5139341599ca0e8e
KAUSSHIK <kausshikmanojkumar@gmail.com>
2023-11-16 19:40:47 -0600

    Make modifications to content

--- script/script.js
+++ script/script.js
@@ -1,9 +1,9 @@
 // add class navbarDark on navbar scroll
-const header = document.querySelector('.navbar');
+const header = document.querySelector('.navbarScroll');
 
 window.onscroll = function() {
     var top = window.scrollY;
-    if(top >=100) {
+    if (top >= 100) {
         header.classList.add('navbarDark');
     }
     else {
